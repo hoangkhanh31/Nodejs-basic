07d855c18bb66bed56e85cc505252017833cf940
hoangkhanh31git config --global user.name hoangkhanh31git config --global user.name hoangkhanh31 <hoangkhanhdragon@gmail.com>
2024-06-03 16:59:19 +0700

    database connection

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');  //commonjs     //--or-- import express from 'express';    // es modules
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web')
+const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
 const app = express();   // app express
 const port = process.env.PORT || 8888;       //port
@@ -10,9 +11,16 @@ const hostname = process.env.HOST_NAME;
 //config template engine
 configViewEngine(app);
 
-//Khai báo route
-app.use('/',webRoutes);
+connection.execute(
+    'SELECT * FROM Users',
+    ['Rick C-137', 53],
+    function (err, results, fields) {
+        console.log(results); 
+    }
+);
 
+//Khai báo route
+app.use('/', webRoutes);
 
 
 app.listen(port, hostname, () => {
